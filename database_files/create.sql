11250ed1b75f74deaa501f4b854ccc20b83ff13b
flyingmeat <wuheng0316@gmail.com>
2016-12-09 15:59:12 -0500

    update database

--- database_files/create.sql
+++ database_files/create.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- please use command 
+-- mysqldump -u root -p recipe > database_dump.txt
+
 --
 -- Table structure for table `Recipe`
 --
